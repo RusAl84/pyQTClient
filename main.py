467db22bd562114f799946ab5a3b3832301d06af
Rusal84 <rusal@bk.ru>
2021-03-26 21:34:05 +0300

    add MainWindow Class

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import sys
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    ServerAdress = "http://localhost:5000"
+    MessageID = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('messenger.ui', self)
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
